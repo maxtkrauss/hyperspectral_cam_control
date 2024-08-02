f92b2d27c8e4dca07067ce53de51fc26d4aa3baf
j.dammann <j.dammann@stud.uni-goettingen.de>
2024-08-02 22:43:41 +0200

    added draft for programm infrastructure

---
+++ create_dataset_display.py
@@ -0,0 +1,42 @@
+### Create a dataset from displayed images with the hyperspectral cubert cam and the Thorlabs diffractive cam ###
+
+import pylablib as pll
+import cuvis
+import pygame
+
+# Parameters
+
+display_image_folder = 'images/display'
+
+thorlabs_image_folder = 'images/thorlabs'
+
+cubert_image_folder = 'images/cubert'
+
+
+# Setup the Thorlabs cam
+
+# Setup the the Cubert cam
+
+# Calibrate the Cubert cam
+
+# Set up the pygame display and images
+images = []
+
+for img in images:
+
+    # Display image
+
+    # Take photo with Thorlabs cam
+
+    # Save Thorlabs image
+
+    # Take photo with Cubert cam
+
+    # Save Cubert image
+
+    # wait half a second
+
+    pass
+
+pygame.quit()
+
