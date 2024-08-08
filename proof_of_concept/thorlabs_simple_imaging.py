90df9bd9ef4ac1de3e69cd5f47ccf41e7f983382
Jakob Dammann <jakob.dammannhotmail.de>
2024-08-08 15:30:38 -0600

    comments for average calibration

--- proof_of_concept/thorlabs_simple_imaging.py
+++ proof_of_concept/thorlabs_simple_imaging.py
@@ -8,7 +8,7 @@ import matplotlib.pyplot as plt
 tl.list_cameras_tlcam()
 cam = tl.ThorlabsTLCamera()
 
-cam.set_exposure(100e-3)
+cam.set_exposure(10e-3)
 cam.set_roi(0, 2448, 0, 2408, hbin=1, vbin=1)
 frame = cam.snap()
 
@@ -16,7 +16,17 @@ avg = np.average(frame)
 std = np.std(frame)
 print("shape:", frame.shape, "min:", np.min(frame), "max:", np.max(frame), "std:", std, "avg:", avg)
 
-plt.imshow(frame, vmin=avg-std, vmax=avg+std) # to better view dark frames
+plt.imshow(frame)# vmin=avg-std, vmax=avg+std) # to better view dark frames
 plt.colorbar()
 plt.title("frame")
 plt.show()
+
+
+# Measuring averages of just white background
+# just lens, 69cm from front lens, f2, 10ms: 2315
+# just lens, 69cm from front lens, f2.8, 10ms: 1535
+# just lens, 69cm from front lens, f4, 10ms: 738
+# just lens, 69cm from front lens, f5.6, 10ms: 420 (5x darker than f2)
+# just lens, 69cm from front lens, f8, 10ms: 260
+
+# 4f system, 69cm from front lens, f5.6, 10ms:
